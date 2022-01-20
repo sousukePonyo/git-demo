5f333cddba3ccc2c01f11f23dd06331c134e18a8
liutf <liutf@ndky.com>
2022-01-20 16:23:21 +0800

    my first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("hello,ketty!");
+		System.out.println("hello,jack!");
+		System.out.println("hello,petter!");
+		System.out.println("hello,tom!");
+	}
+}
